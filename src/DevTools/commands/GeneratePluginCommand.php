59852d587a1b68f424a8c0166fb805d6a1f559a9
Dylan K. Taylor <odigiman@gmail.com>
2017-04-27 20:06:08 +0100

    Fixed file header

--- src/DevTools/commands/GeneratePluginCommand.php
+++ src/DevTools/commands/GeneratePluginCommand.php
@@ -1,10 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dylan Taylor
- * Date: 27/04/2017
- * Time: 19:07
- */
+
+/*
+ * DevTools plugin for PocketMine-MP
+ * Copyright (C) 2017 PocketMine Team <https://github.com/pmmp/PocketMine-DevTools>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
 
 namespace DevTools\commands;
 
